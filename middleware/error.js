a8435bec10b864f35e40b7d1684f19e6ed277e74
Brian Ngechu <brian.ngechu@tracom.co.ke>
2021-03-21 22:37:23 +0300

    create error handler middleware

---
+++ middleware/error.js
@@ -0,0 +1,29 @@
+const ErrorResponse = require('../utils/errorResponse');
+
+const errorHandler = (error, req, res, next) => {
+  const errorMsg = { ...error };
+  errorMsg.message = error.message;
+  console.log(error);
+
+  //Mongoose bad object id
+  if (error.name === 'CastError') {
+    const message = ` Bootcamp not found with id of ${error.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+  //Mongoose duplicate object error
+  if (error.code === 11000) {
+    const message = ` Bootcamp with name ${error.keyValue.name} already exists`;
+    error = new ErrorResponse(message, 400);
+  }
+  //Mongoose validation errorHandler
+  if (error.name === 'ValidationError') {
+    const message = Object.values(error.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res
+    .status(error.statusCode || 500)
+    .json({ success: false, error: error.message || 'Server error' });
+};
+
+module.exports = errorHandler;
