78bf541f835ec3feb5f31219efdbe4f8e4863ca8
Brian Ngechu <brian.ngechu@tracom.co.ke>
2021-03-23 21:43:56 +0300

    Create a database seeder

---
+++ seeder.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+//Load env variables
+
+dotenv.config({ path: './config/config.env' });
+
+const Bootcamp = require('./models/Bootcamp');
+
+//Connect to database
+mongoose.connect(process.env.MONGOOSE_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+//Read Json files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log('Data Imported....'.green.inverse);
+    process.exit();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+//Deleting the Data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log('Data Destroyed....'.red.inverse);
+    process.exit();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
