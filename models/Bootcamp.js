0fa3dd7a7bd8e922c0cf40b152f2da6b5909e218
Brian Ngechu <brian.ngechu@tracom.co.ke>
2021-03-29 23:38:27 +0300

    Add time stamp

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -2,105 +2,108 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 const geoCoder = require('../utils/geocoder');
 
-const BootcampSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please add  a name'],
-    unique: true,
-    trim: true,
-    maxLength: [50, 'Name cannot exceed 50 characters'],
-  },
-  slug: String,
-  description: {
-    type: String,
-    required: [true, 'Please add  a description'],
-    maxLength: [500, 'Name cannot exceed 500 characters'],
-  },
-  website: {
-    type: String,
-    match: [
-      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-      'Please use a valid URL sith Http or Https',
-    ],
-  },
-  phone: {
-    type: String,
-    maxLength: [20, 'Phone number cannot exceed 20 characters'],
-  },
-  email: {
-    type: String,
-  },
-  address: {
-    type: String,
-    required: [true, 'Please add  an address'],
-  },
-  location: {
-    //GEO JSON point
+const BootcampSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add  a name'],
+      unique: true,
+      trim: true,
+      maxLength: [50, 'Name cannot exceed 50 characters'],
+    },
+    slug: String,
+    description: {
+      type: String,
+      required: [true, 'Please add  a description'],
+      maxLength: [500, 'Name cannot exceed 500 characters'],
+    },
+    website: {
+      type: String,
+      match: [
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+        'Please use a valid URL sith Http or Https',
+      ],
+    },
+    phone: {
+      type: String,
+      maxLength: [20, 'Phone number cannot exceed 20 characters'],
+    },
+    email: {
+      type: String,
+    },
+    address: {
+      type: String,
+      required: [true, 'Please add  an address'],
+    },
+    location: {
+      //GEO JSON point
 
-    type: {
-      type: String, // Don't do `{ location: { type: String } }`
-      enum: ['Point'], // 'location.type' must be 'Point'
+      type: {
+        type: String, // Don't do `{ location: { type: String } }`
+        enum: ['Point'], // 'location.type' must be 'Point'
+        // required: true,
+      },
+      coordinates: {
+        type: [Number],
+        index: '2dsphere',
+      },
+      formattedAddress: String,
+      street: String,
+      city: String,
+      state: String,
+      zipcode: String,
+    },
+    careers: {
+      //Array of strings
+      type: [String],
+      required: true,
+      enum: [
+        'Web Development',
+        'Mobile Development',
+        'UI/UX',
+        'Data Science',
+        'Business',
+        'Other',
+      ],
+    },
+    averageRating: {
+      type: Number,
+      min: [1, 'Rating must be atleast 1'],
+      max: [10, 'Rating must be atmost 10'],
+    },
+    averageCost: Number,
+    photo: {
+      type: String,
+      default: 'no-photo.jpg',
+    },
+    housing: {
+      type: Boolean,
+      default: false,
+    },
+    jobAssistance: {
+      type: Boolean,
+      default: false,
+    },
+    jobGuarantee: {
+      type: Boolean,
+      default: false,
+    },
+    acceptGi: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
       // required: true,
     },
-    coordinates: {
-      type: [Number],
-      index: '2dsphere',
-    },
-    formattedAddress: String,
-    street: String,
-    city: String,
-    state: String,
-    zipcode: String,
   },
-  careers: {
-    //Array of strings
-    type: [String],
-    required: true,
-    enum: [
-      'Web Development',
-      'Mobile Development',
-      'UI/UX',
-      'Data Science',
-      'Business',
-      'Other',
-    ],
-  },
-  averageRating: {
-    type: Number,
-    min: [1, 'Rating must be atleast 1'],
-    max: [10, 'Rating must be atmost 10'],
-  },
-  averageCost: Number,
-  photo: {
-    type: String,
-    default: 'no-photo.jpg',
-  },
-  housing: {
-    type: Boolean,
-    default: false,
-  },
-  jobAssistance: {
-    type: Boolean,
-    default: false,
-  },
-  jobGuarantee: {
-    type: Boolean,
-    default: false,
-  },
-  acceptGi: {
-    type: Boolean,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    // required: true,
-  },
-});
+  { timestamps: true }
+);
 //Create Bootcamp slug from the name
 BootcampSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
