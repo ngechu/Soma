29354bb080afedcb3dcf6806c8fbda112f31a5e4
Brian Ngechu <brian.ngechu@tracom.co.ke>
2021-04-28 23:14:52 +0300

    Clean up code to use resource

--- controllers/courses.js
+++ controllers/courses.js
@@ -9,18 +9,15 @@ const asyncHandler = require("../middleware/async");
 //@access public
 
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  let query;
   if (req.params.bootcampId) {
-    query = Course.find({ bootcamp: req.params.bootcampId });
-  } else {
-    query = Course.find().populate({
-      path: "bootcamp",
-      select: "name description",
-    });
-  }
+    const courses = await Course.find({ bootcamp: req.params.bootcampId });
 
-  const courses = await query;
-  res.status(200).json({ success: true, count: courses.length, data: courses });
+    return res
+      .status(200)
+      .json({ success: true, count: courses.length, data: courses });
+  } else {
+    res.status(200).json(res.advancedResults);
+  }
 });
 
 //@desc Get single Course
