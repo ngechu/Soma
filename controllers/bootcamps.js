8917db8b8912fcd6b89692e28ec9fa9da1f08f38
Brian Ngechu <brian.ngechu@tracom.co.ke>
2021-04-28 23:12:27 +0300

    Clean up code for reusing in other modules

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,76 +8,7 @@ const asyncHandler = require("../middleware/async");
 //@route GET /api/v1/bootcamps
 //@access public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  let query;
-
-  //Copy req.query
-  let reqQuery = { ...req.query };
-
-  //Exclude fields
-  const removeFields = ["select", "sort", "page", "limit"];
-
-  //Loop over the query params and remove the fields to be excluded
-  removeFields.forEach((params) => delete reqQuery[params]);
-
-  //Create the query string
-  let queryStr = JSON.stringify(reqQuery);
-
-  //Create the operators to query within($gte)
-
-  queryStr = queryStr.replace(
-    /\b(gt|gte|lt|lte|in)\b/g,
-    (match) => `$${match}`
-  );
-  //Finding the resource
-  query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
-
-  //Select Fields to show
-  if (req.query.select) {
-    const fields = req.query.select.split(",").join(" ");
-
-    query = query.select(fields);
-  }
-
-  //Sort
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(",").join(" ");
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort("-createdAt");
-  }
-
-  //Pagination
-
-  const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 25;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const total = await Bootcamp.countDocuments();
-  query = query.skip(startIndex).limit(limit);
-
-  //Executing the query
-  const bootcamps = await query;
-
-  //Pagination results
-  const pagination = {};
-  if (endIndex < total) {
-    pagination.next = {
-      page: page + 1,
-      limit,
-    };
-    if (startIndex > 0) {
-      pagination.prev = {
-        page: page - 1,
-        limit,
-      };
-    }
-  }
-  res.status(200).json({
-    success: true,
-    count: bootcamps.length,
-    pagination,
-    data: bootcamps,
-  });
+  res.status(200).json(res.advancedResults);
 });
 
 //@desc Get a Bootcamp
