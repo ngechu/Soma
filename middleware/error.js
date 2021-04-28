9fa0b49b05060d9dc7b43f0234a25e9fd0216bba
Brian Ngechu <brian.ngechu@tracom.co.ke>
2021-04-29 00:10:01 +0300

    Clean up code

--- middleware/error.js
+++ middleware/error.js
@@ -1,29 +1,29 @@
-const ErrorResponse = require('../utils/errorResponse');
+const ErrorResponse = require("../utils/errorResponse");
 
 const errorHandler = (error, req, res, next) => {
   const errorMsg = { ...error };
   errorMsg.message = error.message;
-  console.log(error);
 
   //Mongoose bad object id
-  if (error.name === 'CastError') {
+  if (error.name === "CastError") {
     const message = ` Bootcamp not found with id of ${error.value}`;
     error = new ErrorResponse(message, 404);
   }
   //Mongoose duplicate object error
   if (error.code === 11000) {
-    const message = ` Bootcamp with name ${error.keyValue.name} already exists`;
+    console.log(error);
+    const message = ` Bootcamp with email ${error.keyValue.email} already exists`;
     error = new ErrorResponse(message, 400);
   }
   //Mongoose validation errorHandler
-  if (error.name === 'ValidationError') {
+  if (error.name === "ValidationError") {
     const message = Object.values(error.errors).map((val) => val.message);
     error = new ErrorResponse(message, 400);
   }
 
   res
     .status(error.statusCode || 500)
-    .json({ success: false, error: error.message || 'Server error' });
+    .json({ success: false, error: error.message || "Server error" });
 };
 
 module.exports = errorHandler;
